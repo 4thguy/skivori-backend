ca02da721839b7a2d34ca43529ef35c8618729bf
4thguy <ivanxuereb@gmail.com>
2025-01-31 13:35:41 +0100

    basic express app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World')
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
