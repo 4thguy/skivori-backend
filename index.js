8348801cf8aebb1257362d1a22a84458ea8ec588
4thguy <ivanxuereb@gmail.com>
2025-01-31 14:05:12 +0100

    add game data

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+let gameDataPromise;
+
+function readGameData() {
+    if (!gameDataPromise) {
+        gameDataPromise = new Promise((resolve, reject) => {
+            fs.readFile('./data/game-data.json', 'utf8', (err, data) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                try {
+                    resolve(JSON.parse(data));
+                } catch (parseError) {
+                    reject(parseError);
+                }
+            });
+        });
+    }
+    return gameDataPromise;
+}
+
+app.get('/data/games', function (req, res) {
+    readGameData()
+        .then(data => res.json(data))
+        .catch(err => {
+            res.status(500);
+            res.send(err);
+            console.log(err);
+        });
 });
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port);
